29dd971e61bbbb082b1425872ec81e183b05831c
Azadpour <eazadpour@usgs.gov>
2022-08-29 09:54:19 -0700

    updated _targets.R to pull out the state's information before passing it to the next step in tar_map()

--- _targets.R
+++ _targets.R
@@ -22,9 +22,8 @@ list(
 
   tar_map(
     values = tibble(state_abb = states),
-    tar_target(nwis_data, get_site_data(oldest_active_sites, state_abb, parameter))
-    # Insert step for tallying data here
-    # Insert step for plotting data here
+    tar_target(nwis_inventory, get_state_inventory(sites_info = oldest_active_sites, state_abb)),
+    tar_target(nwis_data, get_site_data(nwis_inventory, state_abb, parameter))
   ),
 
 
