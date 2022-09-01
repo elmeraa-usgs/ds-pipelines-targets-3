d0a63696fa483fb234cc47e2a2445651ca4f1a89
Azadpour <eazadpour@usgs.gov>
2022-09-01 10:17:27 -0700

    Switched to dynamic branching following issue #12, removed tar_map() calls and combiners targets

--- _targets.R
+++ _targets.R
@@ -11,7 +11,7 @@ tar_option_set(packages = c("tidyverse", "dataRetrieval", "urbnmapr", "rnaturale
 source("1_fetch/src/find_oldest_sites.R")
 source("1_fetch/src/get_site_data.R")
 source("2_process/src/tally_site_obs.R")
-source("2_process/src/summarize_targets.R")
+source("3_summarize/src/summarize_targets.R")
 source("3_visualize/src/map_sites.R")
 source("3_visualize/src/plot_site_data.R")
 source("3_visualize/src/plot_data_coverage.R")
@@ -24,42 +24,49 @@ states <- c('AL','AZ','AR','CA','CO','CT','DE','DC','FL','GA','ID','IL','IN','IA
             'UT','VT','VA','WA','WV','WI','WY','AK','HI','GU','PR')
 parameter <- c('00060')
 
-# Static Branching
-mapped_by_state_targets <- tar_map(
-  unlist=FALSE,
-    values = tibble(state_abb = states) %>%
-      mutate(state_plot_files = sprintf("3_visualize/out/timeseries_%s.png", state_abb)),
-    names = state_abb,
-    tar_target(nwis_inventory, filter(oldest_active_sites, state_cd == state_abb)),
-    tar_target(nwis_data, retry(get_site_data(nwis_inventory, state_abb, parameter), when = "Ugh, the internet data transfer failed! Try again.", max_tries = 30)),
-    tar_target(tally, tally_site_obs(nwis_data)),
-    tar_target(timeseries_png, plot_site_data(state_plot_files, nwis_data, parameter),
-               format = "file")
-  )
 
+# Targets
 list(
   # Identify oldest sites
   tar_target(oldest_active_sites, find_oldest_sites(states, parameter)),
 
-  mapped_by_state_targets,
+  # Splitter target
+  tar_target(nwis_inventory,
+             oldest_active_sites %>%
+               group_by(state_cd) %>%
+               tar_group(),
+             iteration = "group"),
 
-  # Add combiner
-  tar_combine(obs_tallies, mapped_by_state_targets$tally,
-              command = combine_obs_tallies(!!!.x)),
+  # Download the data
+  tar_target(nwis_data,
+             retry(get_site_data(nwis_inventory, nwis_inventory$state_cd, parameter),
+                   when = "Ugh, the internet data transfer failed! Try again.", max_tries = 30),
+             pattern = map(nwis_inventory)),
 
-  # Summary combiner
-  tar_combine(
-    summary_state_timeseries_csv,
-    mapped_by_state_targets$timeseries_png,
-    command = summarize_targets('3_visualize/log/summary_state_timeseries.csv', !!!.x),
-    format="file"
-  ),
+  # Tallying
+  tar_target(tally,
+             tally_site_obs(nwis_data),
+             pattern = map(nwis_data)),
 
   # Plotting data coverage
   tar_target(
     plot_data_coverage_png,
-    plot_data_coverage(obs_tallies, "3_visualize/out/data_coverage.png", parameter),
+    plot_data_coverage(tally, "3_visualize/out/data_coverage.png", parameter),
     format = "file"
+  ),
+
+
+  # Timeseries pngs
+  tar_target(timeseries_png, plot_site_data(sprintf("3_visualize/out/timeseries_%s.png", unique(nwis_data$State)), nwis_data, parameter),
+             pattern = map(nwis_data),
+             format = "file"),
+
+
+  # Summary time series
+  tar_target(
+    summary_state_timeseries_csv,
+    command = summarize_targets('3_visualize/log/summary_state_timeseries.csv', names(timeseries_png)),
+    format="file"
   ),
 
 
@@ -76,5 +83,4 @@ list(
     map_timeseries(oldest_active_sites, summary_state_timeseries_csv, "3_visualize/out/timeseries_map.html"),
     format = "file"
   )
-
 )
